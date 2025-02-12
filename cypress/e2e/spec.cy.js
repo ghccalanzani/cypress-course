e3c9d960604ecde956b16dce04ba04b70ba5392a
Gustavo Calanzani <gustavo.calanzani@autoglass.com.br>
2025-02-12 13:49:00 -0300

    inicial

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,14 @@
+describe('Meu primeiro teste', () => {
+  it('funciona corretamente', () => {
+    //Visitar site
+    cy.visit('https://example.cypress.io')
+    //Verificar se existe e clicar
+    cy.contains('type').click() //(Cypress interrompe comandos e espera até 60s para nova página carregar ao mudar de página)
+    //Verificar se URL possui caminho especificado
+    cy.url().should('include', '/commands/actions')
+    //Encontrar elemento (classe) e digitar
+    cy.get('.action-email').type('fake@email.com')
+    //Verificar se foi digitado corretamente
+    cy.get('.action-email').should('have.value', 'fake@email.com')
+  })
+})
